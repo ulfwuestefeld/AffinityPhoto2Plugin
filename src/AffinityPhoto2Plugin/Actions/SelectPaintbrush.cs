26844c79a914fb6b4d9d294ae7601b2530a70691
Ulf Wüstefeld <ulf@wuesti.eu>
2022-12-30 12:29:05 +0100

    Added Button toggling and Images for paint brush.

--- src/AffinityPhoto2Plugin/Actions/SelectPaintbrush.cs
+++ src/AffinityPhoto2Plugin/Actions/SelectPaintbrush.cs
@@ -1,19 +1,31 @@
 ﻿namespace Loupedeck.AffinityPhoto2Plugin
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
-    class SelectPaintbrush : PluginDynamicCommand
+    public class SelectPaintbrush : PluginDynamicCommand
     {
+        private Boolean _toggleState = false;
+
+        private readonly String _image0ResourcePath;
+        private readonly String _image1ResourcePath;
+
         public SelectPaintbrush() : base(displayName: "Paintbrush", description: "Selects the paint brush.", groupName: "Brushes")
         {
+            this._image0ResourcePath = EmbeddedResources.FindFile("SelectPaintbrush0.png");
+            this._image1ResourcePath = EmbeddedResources.FindFile("SelectPaintbrush1.png");
+        }
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            return !this._toggleState
+                ? EmbeddedResources.ReadImage(this._image0ResourcePath)
+                : EmbeddedResources.ReadImage(this._image1ResourcePath);
         }
         protected override void RunCommand(String actionParameter)
         {
+            
             this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyB);
+            this._toggleState = !this._toggleState;
+            this.ActionImageChanged();
         }
     }
 }
