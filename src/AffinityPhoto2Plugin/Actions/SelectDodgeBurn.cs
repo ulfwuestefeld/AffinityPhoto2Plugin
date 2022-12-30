481ac7fac296f1368228c778c22fdb708837562e
Ulf Wüstefeld <ulf@wuesti.eu>
2022-12-30 23:00:40 +0100

    Added dodge & burn

---
+++ src/AffinityPhoto2Plugin/Actions/SelectDodgeBurn.cs
@@ -0,0 +1,39 @@
+﻿namespace Loupedeck.AffinityPhoto2Plugin
+{
+    using System;
+
+    public class SelectDodgeBurn : PluginDynamicCommand
+    {
+        //private Boolean _toggleState = false;
+
+        private readonly String _image0ResourcePath;
+        //private readonly String _image1ResourcePath;
+
+        public SelectDodgeBurn() : base(displayName: "Dodge & Burn", description: "Toggles through the dodge and burn functions.", groupName: "Tools Panel")
+        {
+            this._image0ResourcePath = EmbeddedResources.FindFile("SelectDodgeBurn0.png");
+            //this._image1ResourcePath = EmbeddedResources.FindFile("SelectDodgeBurn1.png");
+        }
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            /*return !this._toggleState
+                ? EmbeddedResources.ReadImage(this._image0ResourcePath)
+                : EmbeddedResources.ReadImage(this._image1ResourcePath);*/
+            return EmbeddedResources.ReadImage(this._image0ResourcePath);
+        }
+        protected override void RunCommand(String actionParameter)
+        {
+            /*if(!this._toggleState)
+            {
+                this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyH);
+            }
+            else
+            {
+                this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyB);
+            }
+            this._toggleState = !this._toggleState;*/
+            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyO);
+            this.ActionImageChanged();
+        }
+    }
+}
