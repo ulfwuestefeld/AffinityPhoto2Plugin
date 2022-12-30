b27cad0c93139b74bb1f11c19a8eb6bbaf9cb8b8
Ulf Wüstefeld <ulf@wuesti.eu>
2022-12-30 15:16:41 +0100

    Changed the toggle behavior and display of the selections tools and added toggling through the blemish removal tools. Both behave the same.

---
+++ src/AffinityPhoto2Plugin/Actions/SelectBlemishRemovalTool.cs
@@ -0,0 +1,31 @@
+﻿namespace Loupedeck.AffinityPhoto2Plugin
+{
+    using System;
+
+    public class SelectBlemishRemovalTool : PluginDynamicCommand
+    {
+        //private Boolean _toggleState = false;
+
+        private readonly String _image0ResourcePath;
+        //private readonly String _image1ResourcePath;
+
+        public SelectBlemishRemovalTool() : base(displayName: "Blemish removal Tools", description: "Toggles through the blemish removal tools.", groupName: "Tools Panel")
+        {
+            this._image0ResourcePath = EmbeddedResources.FindFile("SelectBlemishRemovalTool0.png");
+            //this._image1ResourcePath = EmbeddedResources.FindFile("SelectBlemishRemovalTool1.png");
+        }
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            //return !this._toggleState
+            //    ? EmbeddedResources.ReadImage(this._image0ResourcePath)
+            //    : EmbeddedResources.ReadImage(this._image1ResourcePath);
+            return EmbeddedResources.ReadImage(this._image0ResourcePath);
+        }
+        protected override void RunCommand(String actionParameter)
+        {
+            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyJ);
+            //this._toggleState = !this._toggleState;
+            this.ActionImageChanged();
+        }
+    }
+}
--- src/AffinityPhoto2Plugin/Actions/SelectSelectionTool.cs
+++ src/AffinityPhoto2Plugin/Actions/SelectSelectionTool.cs
@@ -6,24 +6,24 @@
     public class SelectSelectionTool : PluginDynamicCommand
     {
         private Boolean _toggleState = false;
-        private Boolean _paintingpixelselections = true;
-        Int32 _togglecount = 0;
+        /*private Boolean _paintingpixelselections = true;
+        Int32 _togglecount = 0;*/
 
         private readonly String _image0ResourcePath;
-        private readonly String _image1ResourcePath;
+        /*private readonly String _image1ResourcePath;
         private readonly String _image2ResourcePath;
-        private readonly String _image3ResourcePath;
+        private readonly String _image3ResourcePath;*/
 
         public SelectSelectionTool() : base(displayName: "Selection Tools", description: "Toogles between painting pixel selection tool and flooding pixel selection tool.", groupName: "Tools Panel")
         {
             this._image0ResourcePath = EmbeddedResources.FindFile("SelectSelectionTool0.png");
-            this._image1ResourcePath = EmbeddedResources.FindFile("SelectSelectionTool1.png");
+            /*this._image1ResourcePath = EmbeddedResources.FindFile("SelectSelectionTool1.png");
             this._image2ResourcePath = EmbeddedResources.FindFile("SelectSelectionTool2.png");
-            this._image3ResourcePath = EmbeddedResources.FindFile("SelectSelectionTool3.png");
+            this._image3ResourcePath = EmbeddedResources.FindFile("SelectSelectionTool3.png");*/
         }
         protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
         {
-            if (!this._toggleState && this._paintingpixelselections)
+            /*if (!this._toggleState && this._paintingpixelselections)
             {
                 return EmbeddedResources.ReadImage(this._image0ResourcePath);
             }
@@ -51,14 +51,15 @@
                         }
                     }
                 }
-            }
+            }*/
+            return EmbeddedResources.ReadImage(this._image0ResourcePath);
         }
         protected override void RunCommand(String actionParameter)
         {
             this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyW);
             this._toggleState = !this._toggleState;
-            if()
-            _togglecount++;
+            //if()
+            //_togglecount++;
             this.ActionImageChanged();
         }
     }
