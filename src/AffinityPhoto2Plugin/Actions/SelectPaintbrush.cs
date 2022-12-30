4ce6171dbd91c65a289fa9e9fe920b1d1cc8d085
Ulf Wüstefeld <ulf@wuesti.eu>
2022-12-30 11:07:58 +0100

    Changed icons and added demo function for selecting a paint brush

---
+++ src/AffinityPhoto2Plugin/Actions/SelectPaintbrush.cs
@@ -0,0 +1,19 @@
+﻿namespace Loupedeck.AffinityPhoto2Plugin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class SelectPaintbrush : PluginDynamicCommand
+    {
+        public SelectPaintbrush() : base(displayName: "Paintbrush", description: "Selects the paint brush.", groupName: "Brushes")
+        {
+        }
+        protected override void RunCommand(String actionParameter)
+        {
+            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyB);
+        }
+    }
+}
