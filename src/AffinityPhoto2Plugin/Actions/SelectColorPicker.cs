159aaf261531275d8a684eb62bdb02e18a1a6641
Ulf Wüstefeld <ulf@wuesti.eu>
2022-12-30 15:29:04 +0100

    removed toggling of brush and color picker because it does not make any sense.

--- src/AffinityPhoto2Plugin/Actions/SelectColorPicker.cs
+++ src/AffinityPhoto2Plugin/Actions/SelectColorPicker.cs
@@ -4,25 +4,26 @@
 
     public class SelectColorPicker : PluginDynamicCommand
     {
-        private Boolean _toggleState = false;
+        //private Boolean _toggleState = false;
 
         private readonly String _image0ResourcePath;
-        private readonly String _image1ResourcePath;
+        //private readonly String _image1ResourcePath;
 
         public SelectColorPicker() : base(displayName: "Colorpicker", description: "Selects the color picker tool.", groupName: "Tools Panel")
         {
             this._image0ResourcePath = EmbeddedResources.FindFile("SelectColorPicker0.png");
-            this._image1ResourcePath = EmbeddedResources.FindFile("SelectColorPicker1.png");
+            //this._image1ResourcePath = EmbeddedResources.FindFile("SelectColorPicker1.png");
         }
         protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
         {
-            return !this._toggleState
+            /*return !this._toggleState
                 ? EmbeddedResources.ReadImage(this._image0ResourcePath)
-                : EmbeddedResources.ReadImage(this._image1ResourcePath);
+                : EmbeddedResources.ReadImage(this._image1ResourcePath);*/
+            return EmbeddedResources.ReadImage(this._image0ResourcePath);
         }
         protected override void RunCommand(String actionParameter)
         {
-            if (!this._toggleState)
+            /*if (!this._toggleState)
             {
                 this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyH);
             }
@@ -30,7 +31,8 @@
             {
                 this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyI);
             }
-            this._toggleState = !this._toggleState;
+            this._toggleState = !this._toggleState;*/
+            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyI);
             this.ActionImageChanged();
         }
     }
--- src/AffinityPhoto2Plugin/Actions/SelectPaintbrush.cs
+++ src/AffinityPhoto2Plugin/Actions/SelectPaintbrush.cs
@@ -4,25 +4,26 @@
 
     public class SelectPaintbrush : PluginDynamicCommand
     {
-        private Boolean _toggleState = false;
+        //private Boolean _toggleState = false;
 
         private readonly String _image0ResourcePath;
-        private readonly String _image1ResourcePath;
+        //private readonly String _image1ResourcePath;
 
         public SelectPaintbrush() : base(displayName: "Paintbrush", description: "Selects the paint brush.", groupName: "Tools Panel")
         {
             this._image0ResourcePath = EmbeddedResources.FindFile("SelectPaintbrush0.png");
-            this._image1ResourcePath = EmbeddedResources.FindFile("SelectPaintbrush1.png");
+            //this._image1ResourcePath = EmbeddedResources.FindFile("SelectPaintbrush1.png");
         }
         protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
         {
-            return !this._toggleState
+            /*return !this._toggleState
                 ? EmbeddedResources.ReadImage(this._image0ResourcePath)
-                : EmbeddedResources.ReadImage(this._image1ResourcePath);
+                : EmbeddedResources.ReadImage(this._image1ResourcePath);*/
+            return EmbeddedResources.ReadImage(this._image0ResourcePath);
         }
         protected override void RunCommand(String actionParameter)
         {
-            if(!this._toggleState)
+            /*if(!this._toggleState)
             {
                 this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyH);
             }
@@ -30,7 +31,8 @@
             {
                 this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyB);
             }
-            this._toggleState = !this._toggleState;
+            this._toggleState = !this._toggleState;*/
+            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyB);
             this.ActionImageChanged();
         }
     }
